fcfa9117843edcdee3983aaa95cf87605692c42b
OliverQin <qlrpeach@gmail.com>
2015-08-23 23:29:40 +0800

    changed port

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 #/usr/bin/env python2
+import os
 from flask import Flask
-
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def hello():
-    return "Hello World!"
-if __name__ ==  '__main__':
-    app.run()
-    
+        return "Hello from Python!"
+
+if __name__ == "__main__":
+        port = int(os.environ.get("PORT", 5000))
+        app.run(host='0.0.0.0', port=port)
